99a915e366ea376a033cc8e257843ac3be20cb3a
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2019-10-22 10:49:10 +0200

    added unit test for controller

---
+++ Brenda.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,58 @@
+using AutoFixture;
+using Brenda.Controllers;
+using Brenda.Jokes;
+using Brenda.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Brenda.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        private readonly WeatherForecastController _controller;
+        private readonly BrendaContext _context;
+        private readonly Mock<IJokeProvider> _jokeProvider;
+
+        public WeatherForecastControllerTests()
+        {
+            var options = new DbContextOptionsBuilder<BrendaContext>()
+                  .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                  .Options;
+            _context = new BrendaContext(options);
+
+            _jokeProvider = new Mock<IJokeProvider>();
+            _controller = new WeatherForecastController(
+                Mock.Of<ILogger<WeatherForecastController>>(), _context, _jokeProvider.Object);
+        }
+
+        [Fact]
+        public async Task ShouldReturnForecast()
+        {
+            Fixture fixture = new Fixture();
+            var q = new List<Forecast>(fixture.CreateMany<Forecast>(25));
+            var jokes = fixture.CreateMany<string>(25).ToArray();
+            _context.Forecasts.AddRange(q);
+            _context.SaveChanges();
+
+            //Act
+            var result = await _controller.GetAsync();
+
+            //Assert
+            int i = 0;
+            var forecasts = q.ToArray();
+            foreach (var item in result)
+            {
+                Assert.Equal(jokes[i], item.Summary);
+                Assert.Equal(forecasts[i].Date, item.Date);
+                Assert.Equal(forecasts[i].TemperatureC, item.TemperatureC);
+                i++;
+            }
+        }
+    }
+}
---
+++ Brenda.Tests/Jokes/JokeProviderTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoFixture;
+using Brenda.Jokes;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Brenda.Tests.Jokes
+{
+    public class JokeProviderTests
+    {
+        private readonly Mock<IHttpClientFactory> _httpClientFactory;
+        private readonly JokeProvider _provider;
+        private readonly Queue<string> _responses;
+
+        public JokeProviderTests()
+        {
+            _httpClientFactory = new Mock<IHttpClientFactory>();
+            StubMessageHandler handler = new StubMessageHandler();
+            _httpClientFactory.Setup(f => f.CreateClient(It.IsAny<string>()))
+                .Returns(new HttpClient(handler));
+            _responses = handler.Responses;
+
+            _provider = new JokeProvider(_httpClientFactory.Object);
+        }
+
+        [Fact]
+        public async Task ShouldReturnJokesInOrder()
+        {
+            Fixture fixture = new Fixture();
+            var responses = fixture.CreateMany<string>(10).ToList();
+            responses.ForEach(r => _responses.Enqueue(r));
+
+            //Act
+            var actual = await _provider.GetJokes(10);
+
+            //Assert
+            Assert.Equal(responses.Count, actual.Length);
+            Assert.Equal(responses, actual);
+        }
+
+        private class StubMessageHandler : HttpMessageHandler
+        {
+            public Queue<string> Responses { get; private set; } = new Queue<string>();
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                if (Responses.TryDequeue(out string response))
+                {
+                    HttpResponseMessage httpResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(response)
+                    };
+                    return await Task.FromResult(httpResponse);
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+    }
+
+}
